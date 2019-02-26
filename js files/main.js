4e3d186c7436bf265da3ad147ca2ce47152476a0
ThimanKrishh <mysoftware15@gmail.com>
2019-02-26 08:23:25 +0530

    Git Test

---
+++ js files/main.js
@@ -0,0 +1,14 @@
+  // alert_message = "This is a alert message from external variable";
+  alert_message = "100";
+  var string = 100;
+  booleanmsg = alert_message === string;
+  alert(typeof booleanmsg);
+  // alert(alert_message === string);
+
+  alert(Number.MIN_VALUE);
+    alert(Number.MAX_VALUE);
+
+
+  var message1;
+  message1 = "Hi there..!!!"
+  console.log(message1);
